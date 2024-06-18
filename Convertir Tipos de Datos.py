ec48243907f2f451d84310f545bfec41fb3989dc
Hazel <149763554+hazelovesuuu@users.noreply.github.com>
2024-06-18 12:18:38 -0600

    Add files via upload

---
+++ Convertir Tipos de Datos.py
@@ -0,0 +1,26 @@
+                     #COVERTIR DATOS DE CADENA DE TEXTO
+
+
+#EJEMPLO NUMERO 1
+
+print("EJEMPLOS DE CADENA DE TEXTO:")
+
+x = 1  #entero
+
+y = 2.0  #flotante
+
+z = "3"   #cadena de texto}
+
+print("RESULTADOS CONVERTIDO A CADENA.")
+
+
+print(str(x))   #convertido a cadena
+print(int(y))   #convertido a entero
+print(float(z)) #convertido a flotante
+
+
+print("RESULTADOS SIN SER CONVERTIDOS A CADENA.")
+
+print(x)
+print(y)
+print(z)
