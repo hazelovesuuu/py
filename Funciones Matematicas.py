ebc8e64f9c94f9641d0f67fc7dfb424d34b20eb3
Hazel <149763554+hazelovesuuu@users.noreply.github.com>
2024-06-20 16:14:56 -0600

    Add files via upload

---
+++ Funciones Matematicas.py
@@ -0,0 +1,87 @@
+                             #FUNCIONES MATEMATICAS
+
+#EJEMPLO NUMERO 1
+
+
+import math   #SE PROCEDE A IMPORTAR LA BIBLIOTECA
+
+PI= 3.1416   # se le asigna la variable y el valor de la misma.
+
+print(round(PI))  # se usa "print" en seguido (round) para redondear el valor.
+
+#LA FUNCION (ROUND) SE USA PARA PODER REDONDEAR LOS VALORES.
+
+
+
+
+#EJEMPLO NUMERO 2
+
+import math
+
+X = 3.14
+
+print(math.ceil(X))
+
+#LA FUNCION (CEIL) SE UTILIZA PARA REDONDEAR EL NUMERO AL MAS CERCANO.
+
+
+
+
+#EJEMPLO NUMERO 3
+
+import math
+
+Y= 3.15
+
+print(abs(Y))
+
+#LA FUNCION (ABS) SE UTILIZA PARA REDONDEAR EL VALOR DE POSITIVO A NEGATIVO O VICEVERSA.
+
+
+
+
+#EJEMPLO NUMERO 4
+
+import math
+
+Z= 20
+
+print(pow(Z,3))
+
+#SE UTILZA LA FUNCION DE (POW) para elevar un numero.
+
+#EJEMPLO NUMERO 5
+
+import math
+
+W= 430
+
+print(math.sqrt(W))
+
+#ESTA FUNCION SE UTILIZA PARA SACAR LA RAIZ CUADRADA DE UN NUMERO
+
+
+#EJEMPLO NUMERO 6
+
+import math
+
+V=3
+T=2
+Y=4
+
+print(max(V,T,Y))
+
+#SE UTLIZA PARA ENCONTRAR EL MAXIMO COMUN MUKTIPLO
+
+#EJEMPLO NUMERO 7
+
+Z= 8
+A= 6
+B= 7
+
+print(min(Z,A,B))
+
+#SE UTLIZA PARA ENCONTRAR EL MINIMO COMUN MULTIPLO
+
+
+
