60bf31ed03b4ece6428a8e08748448cd1b10e1f5
Hazel <149763554+hazelovesuuu@users.noreply.github.com>
2024-07-11 13:02:59 -0600

    Add files via upload

---
+++ Manipular Cadenas .py
@@ -0,0 +1,35 @@
+#MANIPULANDO CADENAS EN PYTHON
+
+#EJEMPLO NUMERO 1
+
+nombre = "Diego Soto Vargas"    #ASIGNACION DE VARIABLES
+
+primer_nombre = nombre[2]  #se crea otra variable (primer_nombre) y se le pone estos [simbolos] [] para poder ubicar la o las letra
+
+print(primer_nombre) #se el pone print y se le adjunta la variaable.
+
+#PARA PODER UBICARLO EN VEZ DE PONER EL CARACTER PONDREMOS LOS NUMEROS PARA UBICARLO.
+
+#EJEMPLO NUMERO 2
+
+nombre1 = "Jimena Gonzalez Lopez"
+
+segundo_nombre= nombre1 [4]
+
+print(segundo_nombre)
+
+#EJEMPLO NUMERO 3
+
+nombre2 = "Delia Vargas Flores"
+
+tercer_nombre= nombre2 [0:12]
+
+print(tercer_nombre)
+
+#EJEMPLO NUMERO 4
+
+nombre3= "Aranza Duran Escalona"
+
+cuarto_nombre= nombre3[:12]
+
+print(cuarto_nombre)
