d795dd844808ef9e66d19f04deeedeeb821e61ef
Hazel <149763554+hazelovesuuu@users.noreply.github.com>
2024-08-28 14:59:07 -0600

    Add files via upload

---
+++ if y else y elif.py
@@ -0,0 +1,28 @@
+                                         #INSTRUCCIONES "IF" Y "ELSE Y ELIF"
+
+                                          #EJEMPLO DE INICIO DE SESION#
+
+#EJEMPLO NUMERO 1
+
+#NUMERO 1
+edad = int(input("¿Cuántos años tienes?")) #variable edad, esta misma se convierte a entero, y se da entrada con el mensaje.
+
+if edad>= 18:   #se crea la condicion y se especifica si es mayor o menor.
+    print("ERES MAYOR DE EDAD!!!")
+else:           #si la condicion es falsa saldra este mensaje "ERES MENOR DE EDAD"
+  print("ERES MENOR DE EDAD")
+
+                                            #///EJERCICIO NUMERO 1//////
+
+print("BIENVENIDO A BAR 28")
+
+valor2=  int(input("POR FAVOR INGRESA TU EDAD !!!"))
+if valor2<= 18:
+    print("ERES MENOR DE EDAD, NO PUEDES ENTRAR")
+elif valor2 ==20:
+     print("ERES MAS QUE BIENVENIDO, ADELANTE!!!")
+else:
+    print("ERES MAYOR DE EDAD, POR FAVOR ENTRA!!!")
+
+#///////////////////////////////////////////////////////////////
+
